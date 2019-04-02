d4ba5287e10017ba04bda492a7df7b287782fd11
rassam001 <aniss.elgharniti@devugees.org>
2019-04-02 19:33:14 +0200

    create exercise

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import { cities } from "./data"; // we imported the array of the cities from the file data.js. you can use in your code the array with the name 'cities'
+
+// here you can type your code
